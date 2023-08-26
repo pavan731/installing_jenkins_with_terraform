38c7c8f9c17ec70e9c004e2d6f1de3758a94f041
pavan rayabagi <68803406+pavan731@users.noreply.github.com>
2023-08-27 01:41:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ provider "aws" {
 }
 
 
-resource "aws_key_pair" "example_keypair" {
-  key_name   = "example-keypair"
+resource "aws_key_pair" "terraform_keypair" {
+  key_name   = "terraform-keypair"
   public_key = tls_private_key.example.public_key_openssh
 }
 
@@ -23,7 +23,7 @@ resource "aws_instance" "app_server" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
 
-  key_name = aws_key_pair.example_keypair.key_name
+  key_name = aws_key_pair.terraform_keypair.key_name
 
   tags = {
     Name = "ec2_server_with_jenkins"
@@ -32,7 +32,12 @@ resource "aws_instance" "app_server" {
 
 output "instance_ip" {
   value = aws_instance.app_server.public_ip
+  
 }
+output "jenkins_website" {
+  value = "(http://${aws_instance.app_server.public_ip}:8080)"
+}
+
 
 resource "tls_private_key" "example" {
   algorithm = "RSA"
@@ -76,7 +81,7 @@ resource "null_resource" "install_jenkins" {
 
 terraform {
   backend "s3" {
-    bucket = "storage010"  # bucket name should be same as the existing
+    bucket = "storage010" # s3 bucket name must be same
     key    = "terraform/state.tfstate"
     region = "us-east-1"  # Change this to your desired region
   }
